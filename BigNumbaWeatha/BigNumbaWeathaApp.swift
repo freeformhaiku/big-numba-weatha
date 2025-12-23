52b23ddde0f86fe3351252fac5081d76eae63f76
arielchu <freeformhaiku@gmail.com>
2025-12-23 17:29:31 -0500

    Initial Commit

---
+++ BigNumbaWeatha/BigNumbaWeathaApp.swift
@@ -0,0 +1,17 @@
+//
+//  BigNumbaWeathaApp.swift
+//  BigNumbaWeatha
+//
+//  Created by Ariel Chu on 12/23/25.
+//
+
+import SwiftUI
+
+@main
+struct BigNumbaWeathaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
